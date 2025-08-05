3e92295d6202388d5bdb559350ae4d6e0f2c61dd
Snehasish Das <dsnehasish73@gmail.com>
2025-08-06 01:15:38 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -14,3 +14,19 @@ document.getElementById("toggleMode").addEventListener("click", () => {
     });
   });
   
+// Dark Mode Toggle
+document.getElementById("toggleMode").addEventListener("click", () => {
+    document.body.classList.toggle("dark");
+  });
+  
+  window.addEventListener('DOMContentLoaded', () => {
+    const progresses = document.querySelectorAll('.progress');
+    progresses.forEach(bar => {
+      const width = bar.style.width;
+      bar.style.width = '0';
+      setTimeout(() => {
+        bar.style.width = width;
+      }, 100);
+    });
+  });
+  
