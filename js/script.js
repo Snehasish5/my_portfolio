fbbcbfbeeb81f9281c020dd936797b90ba1d3ad0
Snehasish Das <dsnehasish73@gmail.com>
2025-08-06 22:40:18 +0530

    initial commit

--- js/script.js
+++ js/script.js
@@ -35,3 +35,22 @@ document.getElementById("toggleMode").addEventListener("click", () => {
       nav.classList.toggle("show");
     }
   }
+
+  let lastScrollTop = 0;
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+      if (scrollTop > lastScrollTop) {
+      // Scrolling down - show navbar
+      navbar.style.top = '0';
+      navbar.style.position = 'fixed';
+      navbar.style.width = '100%';
+      navbar.style.transition = 'top 0.3s';
+      } else {
+      // Scrolling up - hide navbar
+      navbar.style.top = '-80px'; // adjust height to hide fully
+      }
+      lastScrollTop = scrollTop <= 0 ? 0 : scrollTop; // For Mobile or negative scrolling
+  });
